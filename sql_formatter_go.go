b28cc72ab5c5c6b0e2c368fecf60d61ae6e8a3f8
Tohru Takagi <toru.takagi.engineer@gmail.com>
2023-06-17 22:20:41 +0900

    ディレクトリの場合はWalkして全てのファイルを対応する

--- sql_formatter_go.go
+++ sql_formatter_go.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"go/ast"
 	"go/parser"
@@ -10,6 +9,7 @@ import (
 	"go/token"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
 
@@ -17,67 +17,89 @@ import (
 )
 
 func sql_formatter_go_main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Please provide a path to a file or directory")
-		return
+	path := "."
+	if len(os.Args) > 1 {
+		path = os.Args[1]
 	}
-
-	path := os.Args[1]
 
 	switch dir, err := os.Stat(path); {
 	case err != nil:
 		exit(err)
 	case dir.IsDir():
-		exit(errors.New("not implemented"))
+		err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if !info.IsDir() {
+				if !info.IsDir() && !strings.HasPrefix(info.Name(), ".") && strings.HasSuffix(info.Name(), ".go") {
+					formatFile(path)
+				}
+			}
+			return nil
+		})
+		if err != nil {
+			exit(err)
+		}
+
 	default:
 		info, err := os.Stat(path)
 		if err != nil {
 			exit(err)
 		}
 		if !info.IsDir() && !strings.HasPrefix(info.Name(), ".") && strings.HasSuffix(info.Name(), ".go") {
-			fset := token.NewFileSet()
-			astFile, err := parser.ParseFile(fset, path, nil, 0)
-			if err != nil {
-				exit(err)
-			}
+			formatFile(path)
+		}
+	}
+}
 
-			ast.Inspect(astFile, func(n ast.Node) bool {
-				if n == nil {
-					return false
-				}
-				switch x := n.(type) {
-				//インポート、型、変数、定数などの定義。: https://zenn.dev/tenntenn/books/d168faebb1a739/viewer/becd41
-				case *ast.GenDecl:
-					if x.Tok == token.CONST {
-						for _, spec := range x.Specs {
-							vspec := spec.(*ast.ValueSpec)
-							for _, v := range vspec.Values {
-								re := regexp.MustCompile(`^"(.*)"$`)
-								trimSQL := re.ReplaceAllString(strings.Trim(v.(*ast.BasicLit).Value, "`"), "$1")
-								trimSQL = strings.TrimSpace(trimSQL)
-								if strings.HasPrefix(strings.ToUpper(trimSQL), "SELECT") {
-									result, err := formatter.Format(trimSQL)
-									if err != nil {
-										exit(err)
-									}
-									v.(*ast.BasicLit).Value = "`" + result + "`"
-								}
+func formatFile(path string) error {
+	isFormatted := false
+	fset := token.NewFileSet()
+	astFile, err := parser.ParseFile(fset, path, nil, 0)
+	if err != nil {
+		exit(err)
+	}
+
+	ast.Inspect(astFile, func(n ast.Node) bool {
+		if n == nil {
+			return false
+		}
+		switch x := n.(type) {
+		//インポート、型、変数、定数などの定義。: https://zenn.dev/tenntenn/books/d168faebb1a739/viewer/becd41
+		case *ast.GenDecl:
+			if x.Tok == token.CONST {
+				for _, spec := range x.Specs {
+					vspec := spec.(*ast.ValueSpec)
+					for _, v := range vspec.Values {
+						re := regexp.MustCompile(`^"(.*)"$`)
+						trimSQL := re.ReplaceAllString(strings.Trim(v.(*ast.BasicLit).Value, "`"), "$1")
+						trimSQL = strings.TrimSpace(trimSQL)
+						if strings.HasPrefix(strings.ToUpper(trimSQL), "SELECT") {
+							result, err := formatter.Format(trimSQL)
+							if err != nil {
+								exit(err)
 							}
+							v.(*ast.BasicLit).Value = "`" + result + "`"
+							isFormatted = true
 						}
 					}
 				}
-				return true
-			})
-
-			var buf bytes.Buffer
-			if err = printer.Fprint(&buf, fset, astFile); err != nil {
-				exit(err)
-			}
-			if err := ioutil.WriteFile(path, []byte(buf.String()), 0); err != nil {
-				exit(err)
 			}
 		}
+		return true
+	})
+
+	if isFormatted {
+		var buf bytes.Buffer
+		if err = printer.Fprint(&buf, fset, astFile); err != nil {
+			exit(err)
+		}
+		if err := ioutil.WriteFile(path, []byte(buf.String()), 0); err != nil {
+			exit(err)
+		}
 	}
+
+	return nil
 }
 
 func exit(err error) {
